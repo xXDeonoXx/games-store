9fd053d1e7b8704a4a147d13eb0f5cf37911e97d
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-09-30 16:17:39 -0300

    feat: Home Page and all of it's styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from 'next';
+import SelectInput from '../components/atoms/SelectInput';
+// import SelectInput from '../components/atoms/SelectInput';
+import Layout from '../components/Layout';
+import CartView from '../components/organisms/CartView';
+import ProductCard from '../components/organisms/ProductCard';
+import { getProducts } from '../services/api';
+import styles from '../styles/pages/Home.module.scss';
+
+interface HomePageProps {
+  products: [];
+}
+
+const Home: NextPage<HomePageProps> = ({ products }) => {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <section className={styles.wrapper}>
+          <div className={styles.productsWrapper}>
+            <div className={styles.header}>
+              <h1 className={styles.title}>Games</h1>
+              <div className={styles.filterWrapper}>
+                <SelectInput />
+              </div>
+            </div>
+            <div className={styles.productList}>
+              {products.map((product, index) => {
+                return <ProductCard key={product.id} product={product} />;
+              })}
+            </div>
+          </div>
+          <CartView />
+        </section>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
+
+export async function getServerSideProps(context: any) {
+  const products = getProducts();
+  return {
+    props: {
+      products,
+    },
+  };
+}
