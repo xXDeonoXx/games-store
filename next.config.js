1f669e421b46240252aceaed214d69f57a15ae67
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-09-30 16:10:12 -0300

    initial configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    additionalData: `@import "src/styles/common.scss"; @import "src/styles/mixins.scss";`,
+  },
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
