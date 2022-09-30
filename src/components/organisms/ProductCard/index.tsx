4fe8f7ba14aa23ecd9b5765fd5663dd4f6dd2da2
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-09-30 16:41:23 -0300

    feat: hover on product card

--- src/components/organisms/ProductCard/index.tsx
+++ src/components/organisms/ProductCard/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
 import { Product } from '../../../domains/product';
 import ProductImage from '../../atoms/ProductImage';
 import styles from './ProductCard.module.scss';
@@ -8,16 +9,33 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const [hovered, setHovered] = useState(false);
   return (
-    <div className={styles.container}>
-      <div className={styles.imageWrapper}>
-        <div>
-          <ProductImage image={product.image} />
+    <div
+      className={styles.container}
+      onMouseEnter={() => {
+        setHovered(true);
+      }}
+      onMouseLeave={() => {
+        setHovered(false);
+      }}
+    >
+      <Link href={'#'}>
+        <div className={styles.imageWrapper}>
+          <div>
+            <ProductImage image={product.image} />
+          </div>
         </div>
-      </div>
+      </Link>
       <div className={styles.productInfoWrapper}>
-        <span className={styles.productTitle}>{product.name}</span>
-        <span className={styles.productPrice}>R$ {product.price}</span>
+        {hovered ? (
+          <span className={styles.addToCart}>adicionar ao carrinho</span>
+        ) : (
+          <>
+            <span className={styles.productTitle}>{product.name}</span>
+            <span className={styles.productPrice}>R$ {product.price}</span>
+          </>
+        )}
       </div>
     </div>
   );
