0c428aa869762449a6d2072e8ad1b02b07587b16
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-10-02 23:25:01 -0300

    feat: CartPricing Molecule component

---
+++ src/components/molecules/CartPricing/index.tsx
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { ShoppingCartContext } from '../../../context/ShoppingCartContext';
+import { Button } from '../../atoms/Button';
+import LabelWithValue from '../../atoms/LabelWithValue';
+import styles from './CartPricing.module.scss';
+
+const CartPricing = () => {
+  const { products, subTotal, frete } = useContext(ShoppingCartContext);
+
+  if (!products.length) return <></>;
+
+  return (
+    <>
+      <div className={styles.container}>
+        <LabelWithValue label='subtotal' price={subTotal} />
+        <LabelWithValue label='frete' price={subTotal > 200 ? 0 : frete} />
+        <LabelWithValue label='total' price={subTotal + frete} bigPrice />
+      </div>
+      <Button
+        label='finalizar compra'
+        onClick={() => {
+          alert('Compra finalizada com sucesso');
+        }}
+      />
+    </>
+  );
+};
+
+export default CartPricing;
