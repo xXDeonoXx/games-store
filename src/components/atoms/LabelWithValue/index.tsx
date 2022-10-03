16e72a3ec9dd52c870dbb070d63a10af899d6641
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-10-02 23:24:28 -0300

    feat: LabelWithValue component for CartView

---
+++ src/components/atoms/LabelWithValue/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './LabelWithValue.module.scss';
+
+interface LabelWithValueProps {
+  label: string;
+  price: number;
+  bigPrice?: boolean;
+}
+
+const LabelWithValue: React.FC<LabelWithValueProps> = ({
+  label,
+  price,
+  bigPrice,
+}) => {
+  return (
+    <div className={styles.container}>
+      <span className={styles.label}>{label}</span>
+      <span className={`${bigPrice ? styles.bigPrice : styles.price}`}>
+        R$ {price}
+      </span>
+    </div>
+  );
+};
+
+export default LabelWithValue;
