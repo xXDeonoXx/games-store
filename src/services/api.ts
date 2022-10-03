e012a9ee092d8a1869dbe89cce30b308ff205b62
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-10-03 01:04:55 -0300

    feat: getOneProduct api function to get specific product

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import products from '../../products.json';
 export function getProducts() {
   return products;
 }
+
+export function getOneProduct(productId: number) {
+  return products.find((prod) => prod.id == productId);
+}
